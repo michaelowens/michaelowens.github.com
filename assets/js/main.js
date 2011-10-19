8432889e9753ef16a88a07aec09bfd6d8dbde664
Michael Owens <michaelowensnl@gmail.com>
2011-10-19 13:25:23 +0200

    ajax https test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-var api = 'http://github.com/api/v2/json/';
+var api = 'https://github.com/api/v2/json/';
 $(document).ready(function () {
     $.getJSON(api + 'user/show/michaelowens', function (data) {
         console.log(data);
