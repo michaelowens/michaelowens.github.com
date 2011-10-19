939ae5fa96537c553d69bf2a73f0acdf73d43d84
Michael Owens <michaelowensnl@gmail.com>
2011-10-19 14:01:00 +0200

    ajax repos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,23 @@
 var api = 'https://github.com/api/v2/json/',
-    user = {};
+    username = 'michaelowens',
+    user = {},
+    repos = {};
 $(document).ready(function () {
     $('#my_repos').text('0');
     $('#my_private_repos').text('0');
     $('#my_followers').text('0');
-    $.getScript(api + 'user/show/michaelowens?callback=handleUser');
+    $.getScript(api + 'user/show/' + username + '?callback=handleUser');
+    $.getScript(api + 'repos/show/' + username + '?callback=handleRepos');
 });
 
 function handleUser(json) {
     user = json.user;
     $('#my_repos').text(user.public_repo_count);
     $('#my_followers').text(user.followers_count);
+}
 
+function handleRepos(json) {
+    repos = json.repositories;
+    console.log(repos);
     $('.site').fadeIn();
 }
