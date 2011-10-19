2399b3d0fb22cb877dc2fd4886bf9e772cd670c8
Michael Owens <michaelowensnl@gmail.com>
2011-10-19 13:39:01 +0200

    ajax update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
-var api = 'https://github.com/api/v2/json/';
+var api = 'https://github.com/api/v2/json/',
+    user = {};
 $(document).ready(function () {
     $.getScript(api + 'user/show/michaelowens?callback=handleUser');
 });
 
 function handleUser(json) {
-    console.log('incoming');
-    console.log(json);
+    user = json.user;
+    $('#repos').text(user.public_repo_count);
+    $('#fols').text(user.followers_count);
 }
