b47db4adc3581a3d7c13d84e89085b05d6582e78
Michael Owens <michaelowensnl@gmail.com>
2011-10-19 13:44:37 +0200

    id fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 var api = 'https://github.com/api/v2/json/',
     user = {};
 $(document).ready(function () {
+    $('#my_repos').text('0');
+    $('#my_followers').text('0');
     $.getScript(api + 'user/show/michaelowens?callback=handleUser');
 });
 
